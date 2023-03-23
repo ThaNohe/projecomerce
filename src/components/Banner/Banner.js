e49023bacdccd5a32feed9f80a71ad010813eb56
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2023-03-23 16:55:12 -0500

    Visual ok

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,16 +1,7 @@
 import "./Banner.css";
 
 const Banner = () => {
-/*   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState("");
 
-  const fetchData = async () => {
-    const response = await fetch("https://dummyjson.com/products");
-    const products = await response.json();
-
-    console.log(products.products); 
-  };
- */
     return (
       <div className="banner">
         <div className="banner-container">
