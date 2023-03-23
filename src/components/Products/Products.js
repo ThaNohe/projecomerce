44caf140cf10d743a96b56f4d5e7ae3f032c1210
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2023-03-23 16:42:40 -0500

    Cards 6 column - ok

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -5,17 +5,19 @@ const Product = () => {
   const products = useContext(DataContext);
 
   return (
-    <div className="row">
+    <div className="row justify-content-between">
       {products.map((product) => (
-        <div className="col-md-4" key={product.id}>
-          <div className="card mb-4 box-shadow">
+        <div className="col-md-2 col-lg-2 mb-4" key={product.id}>
+          <div className="card h-100 align-items-stretch">
+
             <img
               className="card-img-top"
               src={product.thumbnail}
               alt={product.title}
+              style={{ maxHeight: '200px' }}
             />
 
-            <div className="card-body">
+            <div className="card-body flex-grow-1">
               <h2 className="card-title">{product.title}</h2>
               <p className="card-text">{product.description}</p>
               <button
